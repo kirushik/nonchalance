d7a29fb1a53c05532148e6d1fcda32bb98867957
Kirill Pimenov <kirill@pimenov.cc>
2017-06-05 17:55:18 +0200

    ssl-aware self-contained http client

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 #[macro_use]
 extern crate conrod;
 
-extern crate ttf_noto_sans;
-
-use conrod::{widget, Colorable, Positionable, Widget};
+use conrod::{widget, Colorable, Positionable, Sizeable, Widget};
 use conrod::text::FontCollection;
 use conrod::backend::glium::glium;
 use conrod::backend::glium::glium::{DisplayBuild, Surface};
+
+
+extern crate ttf_noto_sans;
+
+
+extern crate hyper;
+extern crate hyper_rustls;
+
+use hyper::Client;
+use hyper::net::HttpsConnector;
+
 
 pub fn main() {
     const WIDTH: u32 = 400;
@@ -24,8 +33,11 @@ pub fn main() {
     // construct our `Ui`.
     let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
 
+    let mut url: String = "https://ya.ru".into();
+    let mut response_code: String = String::new();
+
     // Generate the widget identifiers.
-    widget_ids!(struct Ids { text });
+    widget_ids!(struct Ids { text, text_box });
     let ids = Ids::new(ui.widget_id_generator());
 
     // Add a `Font` to the `Ui`'s `font::Map` from file.
@@ -37,6 +49,9 @@ pub fn main() {
 
     // The image map describing each of our widget->image mappings (in our case, none).
     let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
+
+    let https_client = Client::with_connector(HttpsConnector::new(hyper_rustls::TlsClient::new()));
+
 
     // Poll events from the window.
     let mut last_update = std::time::Instant::now();
@@ -85,12 +100,28 @@ pub fn main() {
         {
             let ui = &mut ui.set_widgets();
 
-            // "Hello World!" in the middle of the screen.
-            widget::Text::new("Hello World!")
-                .middle_of(ui.window)
+            widget::Text::new(response_code.as_str())
+                .mid_bottom_of(ui.window)
                 .color(conrod::color::WHITE)
                 .font_size(32)
                 .set(ids.text, ui);
+
+            for event in widget::TextBox::new(url.as_str())
+                                        .left_justify()
+                                        .mid_top_of(ui.window)
+                                        .padded_w_of(ui.window, 20.0)
+                                        // .border(1.0)
+                                        // .border_color(conrod::color::WHITE)
+                                        // .color(conrod::color::WHITE)
+                                        .font_size(16)
+                                        .set(ids.text_box, ui)
+                                        {
+                                            use conrod::widget::text_box::Event::*;
+                                            match event {
+                                                Update(new_url) => url = new_url,
+                                                Enter => get_status_code(&https_client, &url, &mut response_code)
+                                            }
+                                        }
         }
 
         // Render the `Ui` and then display it on the screen.
@@ -102,4 +133,10 @@ pub fn main() {
             target.finish().unwrap();
         }
     }
+}
+
+
+fn get_status_code(client: &Client, url: &str, response_code: &mut String) {
+    let res = client.get(url).send().expect("failed to get a resource");
+    *response_code = format!("{}", res.status);
 }
